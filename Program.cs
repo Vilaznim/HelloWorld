30e773a3267ed804c0d19c9470dc0d095e6b12d3
Vilaznim <tnc885@alumni.ku.dk>
2025-04-27 14:23:26 +0200

    Silly console

--- Program.cs
+++ Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, what is your name?");
+
+        string? name = Console.ReadLine();
+
+        Console.WriteLine($"I am going to touch you, {name}");
+
+        Console.WriteLine("Are you scared?");
+
+        Console.ReadLine();
+
+        Console.WriteLine($"You should be...");
     }
 }
