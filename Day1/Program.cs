4678b3e6cb2a0e54bca1a32add0840c58bc05d0c
Vilaznim <tnc885@alumni.ku.dk>
2025-04-27 20:00:36 +0200

    played around with types

---
+++ Day1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    //Day 1
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, what is your name?");
+
+        string? name = Console.ReadLine();
+
+        Console.WriteLine($"I am going to touch you, {name}");
+
+        Console.WriteLine("Are you scared?");
+
+        Console.ReadLine();
+
+        Console.WriteLine($"You should be...");
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    //Day 2
+    static void Main(string[] args) {
+        int a = 5, b = 10;
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
+        //Swap a and b without a third variable
+        Console.WriteLine("Swapping a and b values");
+        (a, b) = (b, a);
+        
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
+        float c = 4.5f, d = 7.1f;
+        Console.WriteLine(c);
+        Console.WriteLine(d);
+
+        char e = 'A', f = 'Z';
+        Console.WriteLine(e);
+        Console.WriteLine(f);
+
+        bool isValid = true, isInvalid = false;
+        Console.WriteLine(isValid);
+        Console.WriteLine(isInvalid);
+
+        string g = "Maren", h = "William";
+        Console.WriteLine(g);
+        Console.WriteLine(h);
+
+        object obj = 123;
+        Console.WriteLine(obj);
+
+        var guess = 42;
+        Console.WriteLine(guess);
+
+        //Celsius to Fahrenheit
+        Console.WriteLine("What is the temperature in Fahrenheit?");
+        Console.Write("Write a number in Celsius!");
+        string? temp = Console.ReadLine();
+
+        if (!double.TryParse(temp, out double celsius)){
+            Console.WriteLine("Whoops, that's not a valid number. Try an actual number!");
+            return;
+        }
+
+        double fahrenheit = celsius * 9.0 / 5.0 + 32;
+        Console.WriteLine($"{celsius}C = {fahrenheit}F");
+
+    }
+}
