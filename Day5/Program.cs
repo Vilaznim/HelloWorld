db9b8372b063421f1c145f1950a0d57919221213
Vilaznim <tnc885@alumni.ku.dk>
2025-04-29 14:12:16 +0200

    added day5: number guessing game

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Guess-the-Number!");
+            PlayGame();
+        }
+
+        // Core game loop
+        static void PlayGame()
+        {
+            var rand = new Random();
+            int secret = rand.Next(1, 51);  // 1–50
+            int guess;
+            int attempts = 0;
+            int hints = -1;
+
+            Console.WriteLine("I have a number between 1 and 50.");
+            do
+            {
+                Console.Write("Your guess: ");
+                string input = Console.ReadLine();
+
+                // TryParsePositive uses an out parameter
+                if (!TryParsePositive(input, out guess))
+                {
+                    Console.WriteLine("Please enter a valid positive integer.");
+                    continue;
+                }
+
+                
+                hints++;
+                attempts++;
+                if (guess < secret)        Console.WriteLine("Too low!");
+                else if (guess > secret)   Console.WriteLine("Too high!");
+                else                       Console.WriteLine("Correct!");
+            }
+            
+            while (guess != secret);
+
+            Console.WriteLine($"You got it in {attempts} attempts and {hints} hints!");
+        }
+
+        // from earlier: parse & validate
+        static bool TryParsePositive(string input, out int result)
+        {
+            if (int.TryParse(input, out int tmp) && tmp > 0)
+            {
+                result = tmp;
+                return true;
+            }
+            result = 0;
+            return false;
+        }
+    }
+}
--- Day5/obj/Debug/net8.0/Day5.AssemblyInfo.cs
+++ Day5/obj/Debug/net8.0/Day5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6815daee578ee3525b0a5b51b1361594d4589e1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1185e044d8f327585ab8219b53e17b26ec44c8df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
